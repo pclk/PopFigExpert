a124462e3d085910b07e489d57f61981e24c4623
Confunius <132553855+Confunius@users.noreply.github.com>
2024-04-09 02:20:42 +0000

    Add swcMinify option to next.config.mjs, install react-textarea-autosize package, and update tailwind.config.js and package-lock.json

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
         "2xl": "1400px",
       },
     },
+    fontFamily: {
+      'inter': ['Inter', 'Poppins', 'sans-serif'],
+    },
     extend: {
       keyframes: {
         "accordion-down": {
